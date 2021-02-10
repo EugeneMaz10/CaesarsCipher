d1749a8a698fbfd49c7ca9165990457ea80e6681
EugeneMaz10 <eugenengaira863@gmail.com>
2021-02-10 16:21:45 +0300

    initial

---
+++ src/Caesar.java
@@ -0,0 +1,110 @@
+import org.apache.tools.ant.types.selectors.SelectSelector;
+
+public class Caesar
+{
+    //Declare a public static String encrypt
+    public static String encrypt(String plainText, int shift)
+    {
+        if (shift > 26) {
+            shift = shift % 26;
+
+        } else if (shift < 0) {
+            shift = (shift % 26) + 26;
+        }
+        String cipherText = "";
+        int length = plainText.length();
+
+        for (int i = 0; i < length; i++) {
+            char ch = plainText.charAt(i);
+            if (Character.isLetter(ch)) {
+                if (Character.isLowerCase(ch)) {
+                    char c = (char) (ch + shift);
+                    if (c > 'z') {
+                        cipherText += (char) (ch - (26 - shift));
+                    } else {
+
+                        cipherText += c;
+                    }
+                } else if (Character.isUpperCase(ch)) {
+                    char c = (char) (ch + shift);
+                    if (c > 'Z') {
+                        cipherText += (char) (ch - (26 - shift));
+                    } else {
+                        cipherText += c;
+                    }
+                }
+                else
+                    {
+                    cipherText += ch;
+                }
+            }
+
+
+
+
+        }
+         return cipherText; 
+    }
+    public static String decrypt(String plainText, int shift)
+    {
+        if (shift>26)
+        {
+            shift = shift % 26;
+
+        } else if (shift < 0) {
+            shift = (shift % 26) + 26;
+        }
+        String cipherText = "";
+        int length = plainText.length();
+
+        for (int i = 0; i < length; i++) {
+            char ch = plainText.charAt(i);
+            if (Character.isLetter(ch)) {
+                if (Character.isLowerCase(ch)) {
+                    char c = (char)(ch - shift);
+                    if (c < 'a') {
+                        cipherText += (char) (ch +(26 - shift));
+                    } else {
+
+                        cipherText += c;
+                    }
+                } else if (Character.isUpperCase(ch)) {
+                    char c = (char) (ch - shift);
+                    if (c < 'A') {
+                        cipherText += (char) (ch + (26 - shift));
+                    } else {
+                        cipherText += c;
+                    }
+                }
+                else
+                {
+                    cipherText += ch;
+                }
+            }
+
+
+
+
+        }
+        return cipherText;
+    }
+
+
+
+
+    public static void main(String[] args)
+        {
+            String text = " THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+            System.out.println("Main Message:");
+            System.out.println(text);
+
+            String cipher = encrypt(text,-3);
+            System.out.println("Encrypted Message:");
+            System.out.println(cipher);
+            System.out.println("Decrypted Message:");
+            String decrypted = decrypt(cipher,-3);
+            System.out.println(decrypted);
+        }
+
+    
+}
